7dcac319752f71b8ad666675a81f5c677d3f4f06
Rindy Portfolio <rindy@tadpole.cc>
2026-03-06 17:01:43 -0500

    Fix dashboard login

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,6 +43,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'is_admin',
     ];
 
     /**
@@ -65,6 +66,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'is_admin' => 'boolean',
         ];
     }
 
